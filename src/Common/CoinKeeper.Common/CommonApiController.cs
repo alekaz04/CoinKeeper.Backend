3c7eeb99c13251fc99abb7697062c9b9755d3bc4
Leonid Vakarin <l.vakarin@intellectika.ru>
2025-04-23 14:14:19 +0000

    --

---
+++ src/Common/CoinKeeper.Common/CommonApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoinKeeper.Common;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CommonApiController : ControllerBase
+{
+}
---
+++ src/Common/CoinKeeper.Common/CommonServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CoinKeeper.Common;
+
+public static class CommonServiceCollectionExtensions
+{
+    public static IServiceCollection AddCommon(this IServiceCollection services)
+    {
+        return services;
+    }
+}
