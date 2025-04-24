de4f03f42452d802f8dce52090de7a6dae14aac7
Leonid Vakarin <l.vakarin@intellectika.ru>
2025-04-24 13:01:38 +0300

    --

--- src/CoinKeeper.Operations/Validators/OperationCreateDtoValidator.cs
+++ src/CoinKeeper.Operations/Validators/OperationCreateDtoValidator.cs
@@ -5,5 +5,9 @@ namespace CoinKeeper.Operations.Validators;
 
 public class OperationCreateDtoValidator : AbstractValidator<OperationCreateDto>
 {
-
+    public OperationCreateDtoValidator()
+    {
+        RuleFor(x => x.Amount).GreaterThanOrEqualTo(0)
+            .WithMessage("Amount must be greater than 0");
+    }
 }
