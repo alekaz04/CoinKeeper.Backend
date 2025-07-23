620899f68ae1b0ba253d050ed8132d477da7cbf6
Leonid Vakarin <l.vakarin@intellectika.ru>
2025-07-23 20:43:12 +0300

    --

--- src/CoinKeeper.Backend.Api/Startup.cs
+++ src/CoinKeeper.Backend.Api/Startup.cs
@@ -12,11 +12,19 @@ public class Startup
     /// <inheritdoc cref="IConfiguration"/>
     private IConfiguration Configuration { get; }
 
+    /// <summary>
+    /// Инициализация приложения
+    /// </summary>
+    /// <param name="configuration"></param>
     public Startup(IConfiguration configuration)
     {
         Configuration = configuration;
     }
 
+    /// <summary>
+    /// Инициализация сервисов
+    /// </summary>
+    /// <param name="services">Коллекция сервисов</param>
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
@@ -30,6 +38,11 @@ public class Startup
         services.AddDbContext<DataContext>(x => x.UseNpgsql(Configuration.GetConnectionString(nameof(DataContext))));
     }
 
+    /// <summary>
+    /// Конфигурация сервисов
+    /// </summary>
+    /// <param name="app"><inheritdoc cref="IApplicationBuilder"/></param>
+    /// <param name="env"><inheritdoc cref="IWebHostEnvironment"/></param>
     public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         app.UseSwagger();
